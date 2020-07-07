93bd683be7624f273461ccf35b23b9a60135e6ee
Jose Alfonso Mora <josealfonsomora@gmail.com>
2020-07-07 11:37:54 +0200

    Change ui to accept only phone number

--- app/src/main/java/com/josealfonsomora/firebasephonenumberauth/ui/login/LoginFormState.kt
+++ app/src/main/java/com/josealfonsomora/firebasephonenumberauth/ui/login/LoginFormState.kt
@@ -3,6 +3,4 @@ package com.josealfonsomora.firebasephonenumberauth.ui.login
 /**
  * Data validation state of the login form.
  */
-data class LoginFormState(val usernameError: Int? = null,
-                          val passwordError: Int? = null,
-                          val isDataValid: Boolean = false)
+data class LoginFormState(val phoneNumberError: Int? = null, val isDataValid: Boolean = false)
